1080e7a7fbea8d90e041464df33ebf2e5e2c066a
maxdoctorkurt@mail.ru <querty123>
2019-01-30 17:05:45 +0300

    layout is ready

--- app/src/main/java/com/example/maxdo/headsandhandstest/MainActivity.kt
+++ app/src/main/java/com/example/maxdo/headsandhandstest/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.maxdo.headsandhandstest
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val passwordEndHelpDrawable = ContextCompat.getDrawable(this, R.drawable.help)
+
+
+
+
+        passwordEt.setCompoundDrawablesWithIntrinsicBounds(null, null, passwordEndHelpDrawable, null)
     }
 }
